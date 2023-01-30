63ea9353e038bbed5805de86072e42fbc3de88e9
Mihail <93585176+GetWeatherOnTuesday@users.noreply.github.com>
2023-01-30 17:47:04 +0000

    f

--- api/index.py
+++ api/index.py
@@ -1,5 +1,25 @@
 from http.server import BaseHTTPRequestHandler
 
+import telebot
+import openai
+
+openai.api_key = "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"
+bot = telebot.TeleBot("5635268086:AAHyqpthIeHd-ytiv88-C43sFDOifNkFX88")
+
+@bot.message_handler(content_types=["text"])
+def handle_message(message):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"{message.text}",
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    bot.send_message(message.chat.id, response.choices[0].text)
+
+bot.polling()
+
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
